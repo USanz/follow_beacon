93a08b295abed260cbf08953fd3dda3218f0c186
Unai Sanz <u.sanz.2019@alumnos.urjc.es>
2023-02-27 16:17:59 +0100

    Better QR boundingbox detection and timeout to start searching qr added.

--- src/qr_code_detector.cpp
+++ src/qr_code_detector.cpp
@@ -30,8 +30,6 @@
 #include <iostream>
 
 
-
-using namespace std::chrono_literals;
 using std::placeholders::_1;
 
 
@@ -47,8 +45,14 @@ public:
     centroid_pub_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(
       "/centroid/rel_pos", 10);
 
-    //timer_ = this->create_wall_timer(
-    //  200ms, std::bind(&QRDetectorNode::timer_callback, this));
+    this->declare_parameter("side_threshold", 5.0);
+    this->declare_parameter("display_gui", true);
+    
+    rclcpp::Parameter side_threshold_param_ = this->get_parameter("side_threshold");
+    rclcpp::Parameter display_gui_param_ = this->get_parameter("display_gui");
+    
+    side_threshold_ = side_threshold_param_.as_double();
+    display_gui_ = display_gui_param_.as_bool();
 
     qrDecoder_ = cv::QRCodeDetector();
   }
@@ -62,10 +66,20 @@ private:
       cv::line(im,
         cv::Point2i(bbox.at<float>(2*i), bbox.at<float>(2*i + 1)),
         cv::Point2i(bbox.at<float>((2*i + 2) % (2*n)), bbox.at<float>((2*i + 3) % (2*n))),
-        cv::Scalar(255,0, 0),
+        cv::Scalar(255, 0, 0),
         2);
     }
-    cv::imshow("Result", im);
+    //two opposite corners to see the rotation.
+    cv::circle(im,
+      cv::Point2i(bbox.at<float>(0), bbox.at<float>(1)),
+      5,
+      cv::Scalar(0, 0, 255),
+      -1);
+    cv::circle(im,
+      cv::Point2i(bbox.at<float>(4), bbox.at<float>(5)),
+      5,
+      cv::Scalar(0, 255, 0),
+      -1);
   }
 
   float get_centroid_and_size(cv::Mat &bbox, std::vector<int> &centroid) {
@@ -82,10 +96,30 @@ private:
     centroid[0] = sum_x / n;
     centroid[1] = sum_y / n;
     
-    //diagonal:
+
+    //test if the bbox is valid (comparing the sides length ~= squared or romboid):
     int x0 = bbox.at<float>(0), y0 = bbox.at<float>(1); // i = 0
     int x2 = bbox.at<float>(4), y2 = bbox.at<float>(5); // i = 2
-    return sqrt(abs(x2-x0)^2 + abs(y2-y0)^2);
+    int x1 = bbox.at<float>(2), y1 = bbox.at<float>(3); // i = 1
+    int x3 = bbox.at<float>(6), y3 = bbox.at<float>(7); // i = 3
+    float dist01 = sqrt((abs(x1-x0)^2) + (abs(y1-y0)^2)); //distance from 0 to 1
+    float dist12 = sqrt((abs(x2-x1)^2) + (abs(y2-y1)^2)); //distance from 1 to 2
+    float dist23 = sqrt((abs(x3-x2)^2) + (abs(y3-y2)^2)); //distance from 2 to 3
+    float dist30 = sqrt((abs(x0-x3)^2) + (abs(y0-y3)^2)); //distance from 3 to 0
+    
+    bool is_valid = (
+      fabs(dist01 - dist12) < side_threshold_ &&
+      fabs(dist12 - dist23) < side_threshold_ &&
+      fabs(dist23 - dist30) < side_threshold_ &&
+      fabs(dist30 - dist01) < side_threshold_);
+
+    float size = -1.0;
+    if (is_valid) {
+      float diag02 = sqrt((abs(x2-x0)^2) + (abs(y2-y0)^2)); //diagonal from 0 to 2
+      float diag13 = sqrt((abs(x3-x1)^2) + (abs(y3-y1)^2)); //diagonal from 1 to 3
+      size = (diag02 + diag13) / 2.0; //average of the diagonals
+    }
+    return size;
   }
 
   void topic_callback(sensor_msgs::msg::Image::SharedPtr msg)
@@ -107,38 +141,51 @@ private:
 
     //the decode part of this function is bugged and it does not work.
     //std::string data = qrDecoder_.detectAndDecode(input_image_, bbox_, rectifiedImage_);
+    std_msgs::msg::Float32MultiArray centroid_per_msg = std_msgs::msg::Float32MultiArray();
+    centroid_per_msg.data = {0.0, 0.0, -1.0};
     bool qr_detected = qrDecoder_.detect(cv_ptr->image, bbox_);
     if (qr_detected) {
-      draw_bbox(cv_ptr->image, bbox_);
-
-      cv::imshow("Result", cv_ptr->image);
-      cv::waitKey(10);
-    
-    
-      std_msgs::msg::Float32MultiArray centroid_per_msg = std_msgs::msg::Float32MultiArray();
-      centroid_per_msg.data = {0.0, 0.0, 0.0};
       std::vector<int> centroid{0, 0};
       float size = get_centroid_and_size(bbox_, centroid);
-      //RCLCPP_INFO(this->get_logger(), "centroid calculated: [%f, %f]", centroid_per_msg.data[0], centroid_per_msg.data[1]);
-      if (size > 0) {
-        centroid_per_msg.data = {
-          (( ( (float) centroid[0]) / cv_ptr->image.cols) - 0.5) * 2.0,
-          (( ( (float) centroid[1]) / cv_ptr->image.rows) - 0.5) * 2.0,
-          size};
-      }
-      RCLCPP_INFO(this->get_logger(), "centroid vals: [%f, %f, %f]", centroid_per_msg.data[0], centroid_per_msg.data[1], centroid_per_msg.data[2]);
-      centroid_pub_->publish(centroid_per_msg);
-    }
 
+      if (display_gui_) {
+        draw_bbox(cv_ptr->image, bbox_);
+        //RCLCPP_INFO(this->get_logger(), "centroid calculated: [%f, %f]", centroid_per_msg.data[0], centroid_per_msg.data[1]);
+        
+        cv::Scalar color = cv::Scalar(0, 0, 255); // red
+        if (size > 0) {
+          centroid_per_msg.data = {
+            (( ( (float) centroid[0]) / cv_ptr->image.cols) - 0.5) * 2.0,
+            (( ( (float) centroid[1]) / cv_ptr->image.rows) - 0.5) * 2.0,
+            size};
+          color = cv::Scalar(0, 255, 0); // green
+        }
+        cv::circle(cv_ptr->image,
+          cv::Point2i(centroid[0], centroid[1]),
+          15, color, -1);
+      }
+    }
+    RCLCPP_INFO(this->get_logger(), "centroid vals: [%f, %f, %f]",
+      centroid_per_msg.data[0],
+      centroid_per_msg.data[1],
+      centroid_per_msg.data[2]);
+
+    centroid_pub_->publish(centroid_per_msg);
+
+    if (display_gui_) {
+      cv::imshow("Result", cv_ptr->image);
+      cv::waitKey(10);
+    }
   }
 
 
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr camera_sub_;
   rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr centroid_pub_;
-  //rclcpp::TimerBase::SharedPtr timer_;
   cv::QRCodeDetector qrDecoder_;
   cv::Mat bbox_, rectifiedImage_;
   
+  float side_threshold_;
+  bool display_gui_;
   cv::Mat input_image_;
 
 };
