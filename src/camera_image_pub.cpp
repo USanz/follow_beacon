0b39575323d4eef1e8102c20a43c3e2b5a18c099
Unai Sanz <u.sanz.2019@alumnos.urjc.es>
2023-03-02 10:53:26 +0100

    Img compression format can be changed from the params file.

--- src/camera_image_pub.cpp
+++ src/camera_image_pub.cpp
@@ -24,20 +24,21 @@ public:
     this->declare_parameter("fps_pub", 15);
     this->declare_parameter("img_resize_width");
     this->declare_parameter("img_resize_height");
+    this->declare_parameter("compression_format");
 
     rclcpp::Parameter cam_num_param = this->get_parameter("cam_num");
     rclcpp::Parameter fps_pub_param = this->get_parameter("fps_pub");
     rclcpp::Parameter img_resize_width_param = this->get_parameter("img_resize_width");
     rclcpp::Parameter img_resize_height_param = this->get_parameter("img_resize_height");
+    rclcpp::Parameter compression_format_param = this->get_parameter("compression_format");
 
     img_resize_width_ = img_resize_width_param.as_int();
     img_resize_height_ = img_resize_height_param.as_int();
 
-
     fps_pub_ = fps_pub_param.as_int();
     int period = 1000 / fps_pub_;
-    std::cout << "publish period: " << period << std::endl;
-    std::cout << "publish fps: " << fps_pub_ << std::endl;
+    RCLCPP_INFO(this->get_logger(), "publish period: %d, fps: %d", period, fps_pub_);
+
     timer_ = this->create_wall_timer(
       std::chrono::milliseconds(period), std::bind(&CameraImagePubNode::timer_callback, this));
 
@@ -45,10 +46,37 @@ public:
     int cam_num = cam_num_param.as_int();
     camera_ = cv::VideoCapture(cam_num);
     if (!camera_.set(cv::CAP_PROP_FPS, fps_pub_)) {
-      RCLCPP_INFO(this->get_logger(), "error setting the camera configuration");
+      RCLCPP_ERROR(this->get_logger(), "error setting the camera configuration");
     }
     if (!camera_.isOpened()) {
-      RCLCPP_INFO(this->get_logger(), "error opening the camera");
+      RCLCPP_ERROR(this->get_logger(), "error opening the camera");
+    }
+
+    std::string format_key = compression_format_param.as_string();
+    std::map<std::string, cv_bridge::Format> compress_format_map{
+      {"BMP",  cv_bridge::Format::BMP},
+      {"DIB",  cv_bridge::Format::DIB},
+      {"JPG",  cv_bridge::Format::JPG},
+      {"JPEG", cv_bridge::Format::JPEG},
+      {"JPE",  cv_bridge::Format::JPE},
+      {"JP2",  cv_bridge::Format::JP2},
+      {"PNG",  cv_bridge::Format::PNG},
+      {"PBM",  cv_bridge::Format::PBM},
+      {"PGM",  cv_bridge::Format::PGM},
+      {"PPM",  cv_bridge::Format::PPM},
+      {"SR",   cv_bridge::Format::SR},
+      {"RAS",  cv_bridge::Format::RAS},
+      {"TIFF", cv_bridge::Format::TIFF},
+      {"TIF",  cv_bridge::Format::TIF}
+    };
+
+    auto search_iter = compress_format_map.find(format_key);
+    if (search_iter != compress_format_map.end()) {
+      RCLCPP_INFO(this->get_logger(), "Image compression format: %s, enum: %d", search_iter->first.c_str(), search_iter->second);
+      compression_format_ = search_iter->second;
+    } else {
+      RCLCPP_ERROR(this->get_logger(), "Image compression format not found");
+        
     }
   }
 
@@ -65,8 +93,8 @@ public:
         std_msgs::msg::Header(),
         "bgr8",
         cam_frame_
-      ).toCompressedImageMsg(cv_bridge::Format::PNG);
-    cam_img_pub_->publish(*cam_img_msg.get());
+      ).toCompressedImageMsg(compression_format_);
+      cam_img_pub_->publish(*cam_img_msg.get());
   }
 
 private:
@@ -80,6 +108,8 @@ private:
   
   cv::Mat cam_frame_;
   cv::VideoCapture camera_;
+
+  cv_bridge::Format compression_format_;
 };
 
 int main(int argc, char * argv[])
