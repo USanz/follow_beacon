15a0112630e25c054673b6b655f8ea7692bef5ea
Unai Sanz <u.sanz.2019@alumnos.urjc.es>
2023-03-24 09:34:48 +0100

    Changes to work using zenoh on the sobot and subcribing from zenoh-flow in the computer.

--- src/zenoh-flow_nodes/source_image.py
+++ src/zenoh-flow_nodes/source_image.py
@@ -3,10 +3,13 @@ from zenoh_flow import Output
 from zenoh_flow.types import Context
 from typing import Any, Dict
 import asyncio
-import cv2
-#from numpy import ndarray, empty, int8
-import numpy as np
 
+import cv2
+import numpy as np
+import json
+
+import zenoh
+from zenoh import Reliability, Sample
 
 class SourceImage(Source):
     def __init__(
@@ -21,39 +24,67 @@ class SourceImage(Source):
         self.rate = float(configuration.get("rate", 15))
         self.img_resize_size = tuple(configuration.get("img_resize_size", []))
         self.quality = int(configuration.get("compression_quality", 90))
-        self.cam_num = int(configuration.get("cam_num", 0))
-        self.camera = cv2.VideoCapture(self.cam_num)
-        if (not self.camera.isOpened()):
-            print("Error opening video stream or file")
-            return None
-            #TODO: is there any way to output an error and exit in zenoh-flow API?
+        #self.cam_num = int(configuration.get("cam_num", 0))
+        #self.camera = cv2.VideoCapture(self.cam_num)
+        #if (not self.camera.isOpened()):
+        #    print("Error opening video stream or file")
+        #    return None
+        #    #TODO: is there any way to output an error and exit in zenoh-flow API?
 
-    def produce_data(self) -> bytes:
-        #get frame:
-        frame_grabbed, frame = self.camera.read()
-
-        if not frame_grabbed:
-            return bytes()
+        zenoh_config_file = str(configuration.get("zenoh_config_file", ""))
+        mode = str(configuration.get("mode", ""))
+        connect = str(configuration.get("connect", ""))
+        listen = str(configuration.get("listen", ""))
+        conf = zenoh.Config.from_file(
+            zenoh_config_file) if zenoh_config_file != "" else zenoh.Config()
+        if mode != "":
+            conf.insert_json5(zenoh.config.MODE_KEY, json.dumps(mode))
+        if connect != "":
+            conf.insert_json5(zenoh.config.CONNECT_KEY, json.dumps(connect))
+        if listen != "":
+            conf.insert_json5(zenoh.config.LISTEN_KEY, json.dumps(listen))
+        self.session = zenoh.open(conf)
         
-        #resize it
-        if self.img_resize_size:
-            resized_frame = cv2.resize(frame, self.img_resize_size, interpolation = cv2.INTER_AREA)
-        else:
-            resized_frame = frame
-        #compress it
-        compressed_frame = img_compress(resized_frame, '.jpg', self.quality)
-        #serialize it
-        return compressed_frame.tobytes()
+        cam_sub_key = str(configuration.get("cam_sub_key", "rt/camera/image_compressed"))
+        self.img = bytes()
+        self.cam_sub = self.session.declare_subscriber(cam_sub_key, self.cam_callback, reliability=Reliability.RELIABLE())
+
+    def cam_callback(self, sample: Sample):
+        self.img = sample.payload #np.frombuffer(sample.payload, dtype=np.uint8)
+        #decimg = cv2.imdecode(sample.payloadencimg, 1)
+        #print(f"received image of size {decimg.size} and shape: {decimg.shape}")
+        #img_height, img_width, img_channels = decimg.shape
+
+    #def produce_data(self) -> bytes:
+    #    #get frame:
+    #    frame_grabbed, frame = self.camera.read()
+    #    if not frame_grabbed:
+    #        return bytes()
+    #    
+    #    #resize it
+    #    if self.img_resize_size:
+    #        resized_frame = cv2.resize(frame, self.img_resize_size, interpolation = cv2.INTER_AREA)
+    #    else:
+    #        resized_frame = frame
+    #    #compress it
+    #    compressed_frame = img_compress(resized_frame, '.jpg', self.quality)
+    #    #serialize it
+    #    return compressed_frame.tobytes()
 
     async def iteration(self) -> None:
         await asyncio.sleep(1.0/self.rate)
-        img = self.produce_data()
-        if img != None:
-            await self.output.send(img)
+        #img = self.produce_data()
+        #if img != None:
+        #    await self.output.send(img)
+        #return None
+        
+        await self.output.send(self.img)
         return None
 
     def finalize(self) -> None: #This is bugged for the moment (it doesn't see the self attributes).
-        self.camera.release()
+        #self.camera.release()
+        self.cam_sub.undeclare()
+        self.session.close()
         return None
 
 def img_compress(img: np.ndarray, format: str, quality: int) -> np.ndarray:
